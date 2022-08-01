b87cad7797ae586b9f1e6b338f481a3ae8701bb4
José Vinicius <vinicius123.vs58@gmail.com>
2022-08-01 05:39:24 -0300

    @Josevinicius53 Desafio Nextsoft

---
+++ PraticaProgramacao/src/repositorios/implementacoes/FamiliaRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using PraticaProgramacao.src.dtos;
+using PraticaProgramacao.src.modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PraticaProgramacao.src.repositorios.implementacoes
+{
+    public class FamiliaRepositorio : IFamilia
+    {
+        public Task AtualizarFamiliaAsync(AtualizarFamiliaDTO Familia)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletarFamiliaAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task NovoFamiliaAsync(NovoFamiliaDTO Familia)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<FamiliaModelo> PegarPeloApto(string apto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<FamiliaModelo> PegarPeloId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<FamiliaModelo>> PegarPeloNome(string nome)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PraticaProgramacao/src/repositorios/implementacoes/MoradorRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using PraticaProgramacao.src.dtos;
+using PraticaProgramacao.src.modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PraticaProgramacao.src.repositorios.implementacoes
+{
+    public class MoradorRepositorio : IMorador
+    {
+        public Task AtualizarMoradorAsync(AtualizarMoradorDTO Morador)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletarMoradorAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task NovoMoradorAsync(NovoMoradorDTO Morador)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<MoradorModelo>> PegarPeloIdadeAsync(string idade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<MoradorModelo> PegarPeloIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<MoradorModelo>> PegarPeloNomeAsync(string nome)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
